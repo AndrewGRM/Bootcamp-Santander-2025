1b6057c50091d33f5c65dee21b0a1ba4fb339d14
AndrewGRM <andrew.grm15@gmail.com>
2025-07-07 12:55:48 -0300

    Implementando camada de dominio

---
+++ src/main/java/com/dio/santander_2025/model/Feature.java
@@ -0,0 +1,10 @@
+package com.dio.santander_2025.model;
+
+import jakarta.persistence.Entity;
+
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+    }
+
---
+++ src/main/java/com/dio/santander_2025/model/News.java
@@ -0,0 +1,9 @@
+package com.dio.santander_2025.model;
+
+import jakarta.persistence.Entity;
+
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
